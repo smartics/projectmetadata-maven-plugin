4551f763928c2e73252f77b4a8757613ccf400a3
robert.reiner <robert.reiner@aaa750ed-2a9a-4131-abc2-cbcc04f95bfd>
2013-09-02 22:52:06 +0000

    [maven-release-plugin]  copy for tag projectmetadata-maven-plugin-0.2.2

---
+++ maven/projectmetadata-maven-plugin/tags/projectmetadata-maven-plugin-0.2.2/src/main/java/de/smartics/maven/plugin/projectmetadata/ArchiveMojo.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2010-2013 smartics, Kronseder & Reiner GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.smartics.maven.plugin.projectmetadata;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.archiver.MavenArchiveConfiguration;
+import org.apache.maven.archiver.MavenArchiver;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.DefaultMavenProjectHelper;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.archiver.jar.JarArchiver;
+
+import de.smartics.maven.util.LoggingUtils;
+
+/**
+ * Collects the project files and collects them to a JAR file that is attached
+ * to the build.
+ *
+ * @goal archive
+ * @phase package
+ * @requiresProject
+ * @description Collects the project files and collects them to a JAR file that
+ *              is attached to the build.
+ * @author <a href="mailto:robert.reiner@smartics.de">Robert Reiner</a>
+ * @version $Revision:591 $
+ */
+public class ArchiveMojo extends AbstractMojo
+{
+  // ********************************* Fields *********************************
+
+  // --- constants ------------------------------------------------------------
+
+  // --- members --------------------------------------------------------------
+
+  // ... Mojo infrastructure ..................................................
+
+  /**
+   * The Maven project.
+   *
+   * @parameter expression="${project}"
+   * @required
+   * @readonly
+   * @since 1.0
+   */
+  private MavenProject project;
+
+  /**
+   * The client Jar archiver.
+   *
+   * @component role="org.codehaus.plexus.archiver.Archiver" roleHint="jar"
+   * @since 1.0
+   */
+  private JarArchiver jarArchiver;
+
+  /**
+   * The archive configuration to use. See <a
+   * href="http://maven.apache.org/shared/maven-archiver/index.html">Maven
+   * Archiver Reference</a>.
+   *
+   * @parameter
+   * @since 1.0
+   */
+  private final MavenArchiveConfiguration archive =
+      new MavenArchiveConfiguration();
+
+  /**
+   * The list of descriptor names to include from the standard folder. Choose
+   * from
+   * <ul>
+   * <li>api</li>
+   * <li>artifact</li>
+   * <li>marker</li>
+   * <li>static-analysis</li>
+   * <li>test</li>
+   * </ul>
+   * Per default all reports are activated.
+   *
+   * @parameter
+   * @since 1.0
+   */
+  private List<String> standardDescriptorNames;
+
+  /**
+   * The list of descriptor names to include from other locations. Please note
+   * that currently only files are supported. Only absolute file names are
+   * supported. Use <tt>${basedir}</tt> to refer to files within the project
+   * folder (which is recommended).
+   *
+   * @parameter
+   * @since 1.0
+   */
+  private List<File> additionalDescriptorFiles;
+
+  // ... Standard properties ..................................................
+
+  /**
+   * A simple flag to skip the generation of the XML files. If set on the
+   * command line use <code>-Dprojectmetadata.skip</code>.
+   *
+   * @parameter expression="${projectmetadata.skip}" default-value="false"
+   * @since 1.0
+   */
+  private boolean skip;
+
+  /**
+   * Specifies the log level used for this plugin.
+   * <p>
+   * Allowed values are <code>SEVERE</code>, <code>WARNING</code>,
+   * <code>INFO</code> and <code>FINEST</code>.
+   * </p>
+   *
+   * @parameter expression="${projectmetadata.logLevel}"
+   * @since 1.0
+   */
+  private String logLevel;
+
+  // ... Specific properties ..................................................
+
+  /**
+   * Specifies whether to attach the generated artifact to the project helper.
+   *
+   * @parameter expression="${attach}" default-value="true"
+   * @since 1.0
+   */
+  private boolean attach;
+
+  /**
+   * The classifier to use to generate the attached artifact.
+   *
+   * @parameter default-value="project"
+   * @since 1.0
+   */
+  private String classifier;
+
+  // ****************************** Initializer *******************************
+
+  // ****************************** Constructors ******************************
+
+  // ****************************** Inner Classes *****************************
+
+  // ********************************* Methods ********************************
+
+  // --- init -----------------------------------------------------------------
+
+  // --- get&set --------------------------------------------------------------
+
+  // --- business -------------------------------------------------------------
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.apache.maven.plugin.AbstractMojo#execute()
+   */
+  public void execute() throws MojoExecutionException, MojoFailureException
+  {
+    if (!skip)
+    {
+      init();
+
+      final File jarFile = createArtifact();
+      if (this.attach)
+      {
+        final MavenProjectHelper helper = new DefaultMavenProjectHelper();
+        helper.attachArtifact(project, jarFile, classifier);
+      }
+    }
+    else
+    {
+      getLog().info("Skipping projectmetadata plugin since skip=true.");
+    }
+  }
+
+  private void init()
+  {
+    LoggingUtils.configureLogger(getLog(), logLevel);
+
+    if (standardDescriptorNames == null)
+    {
+      standardDescriptorNames = new ArrayList<String>();
+      standardDescriptorNames.add("marker");
+      standardDescriptorNames.add("static-analysis");
+      standardDescriptorNames.add("test");
+      standardDescriptorNames.add("artifact");
+      standardDescriptorNames.add("api");
+      // TODO
+    }
+  }
+
+  private File createArtifact() throws MojoExecutionException
+  {
+    final String finalName =
+        project.getBuild().getFinalName() + '-' + classifier + ".jar";
+    final File jarFile = new File(project.getBuild().getDirectory(), finalName);
+    final MavenArchiver clientArchiver = new MavenArchiver();
+    clientArchiver.setArchiver(jarArchiver);
+    clientArchiver.setOutputFile(jarFile);
+
+    try
+    {
+      final ArchiveCreator creator =
+          new ArchiveCreator(project.getBasedir(), new DescriptorSet(
+              standardDescriptorNames, additionalDescriptorFiles));
+      creator.addProjectMetaDataFiles(clientArchiver.getArchiver());
+      clientArchiver.createArchive(project, archive);
+      return jarFile;
+    }
+    catch (final Exception e)
+    {
+      throw new MojoExecutionException(
+          "There was a problem creating the project meta data archive: "
+              + e.getMessage(), e);
+    }
+  }
+
+  // --- object basics --------------------------------------------------------
+
+}
---
+++ maven/projectmetadata-maven-plugin/tags/projectmetadata-maven-plugin-0.2.2/src/test/java/de/smartics/maven/plugin/projectmetadata/MetaDataDescriptorReaderTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2010-2013 smartics, Kronseder & Reiner GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.smartics.maven.plugin.projectmetadata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.smartics.maven.plugin.projectmetadata.bo.FilesDescriptor;
+import de.smartics.maven.plugin.projectmetadata.bo.FilterDescriptor;
+import de.smartics.maven.plugin.projectmetadata.bo.MetaDataDescriptor;
+
+/**
+ * Tests {@link MetaDataDescriptorReader}.
+ * <p>
+ * The test make sure that the XStream library is configured correctly.
+ * </p>
+ *
+ * @author <a href="mailto:robert.reiner@smartics.de">Robert Reiner</a>
+ * @version $Revision:591 $
+ */
+public class MetaDataDescriptorReaderTest
+{
+  // ********************************* Fields *********************************
+
+  // --- constants ------------------------------------------------------------
+
+  // --- members --------------------------------------------------------------
+
+  // ****************************** Inner Classes *****************************
+
+  // ********************************* Methods ********************************
+
+  // --- prepare --------------------------------------------------------------
+
+  // --- helper ---------------------------------------------------------------
+
+  private static MetaDataDescriptorReader createReader(
+      final String descriptorName)
+  {
+    final String basePath = "de/smartics/maven/plugin/projectmetadata";
+    return ProjectTestUtils.createAdditionalReader(basePath, descriptorName);
+  }
+
+  private static void assertMinimal(final MetaDataDescriptor descriptor)
+  {
+    assertEquals("Minimal", descriptor.getName());
+    final FilesDescriptor files = descriptor.getFiles();
+    assertNotNull(files);
+    assertEquals("/target/minimal", files.getProjectLocation());
+    assertEquals("/minimal", files.getArchiveLocation());
+    final FilterDescriptor filter = files.getFilter();
+    assertNull(filter);
+  }
+
+  private static void assertFull(final MetaDataDescriptor descriptor)
+  {
+    assertEquals("Full", descriptor.getName());
+    assertEquals("http://www.example.com/full", descriptor.getHomepage());
+    assertEquals("A descriptor with all fields set.",
+        descriptor.getDescription());
+    assertEquals("category", descriptor.getCategory());
+    assertEquals("subcategory", descriptor.getSubcategory());
+    final FilesDescriptor files = descriptor.getFiles();
+    assertNotNull(files);
+    assertEquals("/target/full", files.getProjectLocation());
+    assertEquals("/category/subcategory/full", files.getArchiveLocation());
+    final FilterDescriptor filter = files.getFilter();
+    assertNotNull(filter);
+
+    final List<String> includes = filter.getIncludes();
+    assertNotNull(includes);
+    assertEquals(2, includes.size());
+    assertEquals("*.xml", includes.get(0));
+    assertEquals("*.csv", includes.get(1));
+
+    final List<String> excludes = filter.getExcludes();
+    assertNotNull(excludes);
+    assertEquals(2, excludes.size());
+    assertEquals("special.xml", excludes.get(0));
+    assertEquals("index.csv", excludes.get(1));
+  }
+
+  // --- tests ----------------------------------------------------------------
+
+  @Test
+  public void emptyDescriptor() throws IOException
+  {
+    final MetaDataDescriptorReader uut = createReader("empty");
+
+    final List<MetaDataDescriptor> result = uut.readMetaDataDescriptors();
+
+    assertTrue("No descriptor expected.", result.isEmpty());
+  }
+
+  @Test
+  public void minimalDescriptor() throws IOException
+  {
+    final MetaDataDescriptorReader uut = createReader("minimal");
+
+    final List<MetaDataDescriptor> result = uut.readMetaDataDescriptors();
+
+    assertEquals("One descriptor expected.", 1, result.size());
+    final MetaDataDescriptor descriptor = result.get(0);
+    assertMinimal(descriptor);
+  }
+
+  @Test
+  public void fullDescriptor() throws IOException
+  {
+    final MetaDataDescriptorReader uut = createReader("full");
+
+    final List<MetaDataDescriptor> result = uut.readMetaDataDescriptors();
+
+    assertEquals("One descriptor expected.", 1, result.size());
+    final MetaDataDescriptor descriptor = result.get(0);
+    assertFull(descriptor);
+  }
+
+  @Test
+  public void twoDescriptor() throws IOException
+  {
+    final MetaDataDescriptorReader uut = createReader("two");
+
+    final List<MetaDataDescriptor> result = uut.readMetaDataDescriptors();
+
+    assertEquals("Two descriptors expected.", 2, result.size());
+    assertMinimal(result.get(0));
+    assertFull(result.get(1));
+  }
+}
